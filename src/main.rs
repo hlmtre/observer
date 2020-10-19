2e41f5c9c486d9c73bba4b64818dd85c39d623fe
Matt Weller <hellmitre@gmail.com>
2020-10-18 21:16:57 -0700

    proof of concept works BETTER

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate sysinfo;
 
 use std::process::Command;
 use std::{thread, time};
-use sysinfo::SystemExt;
+use sysinfo::ProcessStatus;
+use sysinfo::{ProcessExt, SystemExt};
 
 fn main() {
   let trigger_process_name = "htop";
@@ -13,18 +14,33 @@ fn main() {
     let processes = s.get_process_by_name(trigger_process_name);
     let target_processes = s.get_process_by_name(target_process_name);
     println!("looking for process {}", trigger_process_name);
-    if target_processes.len() > 0 {
-      // our target is already running
-      thread::sleep(time::Duration::from_millis(100));
-      continue;
-    }
     if processes.len() > 0 {
       println!("found process {}!", trigger_process_name);
-      println!("spawning process {}!", target_process_name);
-      Command::new(target_process_name)
-        .spawn()
-        .expect("failed to start target process");
+      if target_processes.len() > 0 {
+        for tp in target_processes {
+          match tp.status() {
+            ProcessStatus::Run | ProcessStatus::Sleep | ProcessStatus::Idle => {
+              // our target is already running
+              println!("target already running");
+              thread::sleep(time::Duration::from_millis(5000));
+              continue;
+            }
+            _ => {
+              // any other case...
+              println!("spawning process {}", target_process_name);
+              Command::new(target_process_name)
+                .spawn()
+                .expect("failed to start target process");
+            }
+          }
+        }
+      } else {
+        println!("spawning process {}", target_process_name);
+        Command::new(target_process_name)
+          .spawn()
+          .expect("failed to start target process");
+      }
     }
-    thread::sleep(time::Duration::from_millis(100));
+    thread::sleep(time::Duration::from_millis(5000));
   }
 }
