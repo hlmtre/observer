a944d45cd95242fffcec3ca2efa1c0d6d446211c
Matt Weller <hellmitre@gmail.com>
2020-10-19 23:49:22 -0700

    cleanup. now to figure out how we should set trigger/target process (not hardcoding, obviously...)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 extern crate sysinfo;
 
-use std::process::Command;
-use std::{thread, time};
-use sysinfo::ProcessStatus;
-use sysinfo::{ProcessExt, RefreshKind, SystemExt};
+use std::{process::Command, thread, time};
+use sysinfo::{ProcessExt, ProcessStatus, RefreshKind, SystemExt};
 
 fn main() {
-  let trigger_process_name = "cmd.exe";
-  let target_process_path = "C:\\Windows\\System32\\notepad.exe";
-  let target_process_name = "notepad.exe";
+  let trigger_process_name = "htop";
+  let target_process_path = "pavucontrol";
+  let target_process_name = "pavucontrol";
   let r = RefreshKind::new();
   let mut s = sysinfo::System::new_with_specifics(r.with_processes());
   loop {
@@ -19,7 +17,7 @@ fn main() {
     if processes.len() > 0 {
       println!("found process {}!", trigger_process_name);
       if target_processes.len() > 0 {
-        let tp = target_processes[0];
+        let tp = target_processes[0]; // we're guaranteed to have at least one...
         #[cfg(target_os = "linux")]
         {
           match tp.status() {
@@ -53,10 +51,10 @@ fn main() {
         Command::new(target_process_path)
           .spawn()
           .expect("failed to start target process");
-          // so i don't blow my computer up with ~15GB of chrome instances starting as fast as possible
-          if cfg!(debug_assertions) {
-            thread::sleep(time::Duration::from_millis(1000)); 
-          }
+        // so i don't blow my computer up with ~15GB of chrome instances starting as fast as possible
+        if cfg!(debug_assertions) {
+          thread::sleep(time::Duration::from_millis(1000));
+        }
       }
     } else {
       thread::sleep(time::Duration::from_millis(5000));
