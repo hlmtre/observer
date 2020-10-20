28c5496f884893203afb9021d9525822aa96b072
Matt Weller <hellmitre@gmail.com>
2020-10-20 12:38:36 -0700

    asd

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,31 @@ use std::{process::Command, thread, time};
 use sysinfo::{ProcessExt, ProcessStatus, RefreshKind, SystemExt};
 
 fn main() {
-  let trigger_process_name = "htop";
-  let target_process_path = "pavucontrol";
-  let target_process_name = "pavucontrol";
+  let trigger_process_name = "RainbowSix_Vulkan.exe";
+  let target_process_path = "C:\\Program Files\\obs-studio\\bin\\64bit\\obs64.exe";
+  let target_process_name = "obs64.exe";
   let r = RefreshKind::new();
   let mut s = sysinfo::System::new_with_specifics(r.with_processes());
   loop {
     s.refresh_processes();
     let processes = s.get_process_by_name(trigger_process_name);
     let target_processes = s.get_process_by_name(target_process_name);
-    println!("looking for process {}", trigger_process_name);
+    eprintln!("looking for process {}", trigger_process_name);
     if processes.len() > 0 {
-      println!("found process {}!", trigger_process_name);
+      eprintln!("found process {}!", trigger_process_name);
       if target_processes.len() > 0 {
         let tp = target_processes[0]; // we're guaranteed to have at least one...
         #[cfg(target_os = "linux")]
         {
           match tp.status() {
             ProcessStatus::Run | ProcessStatus::Sleep | ProcessStatus::Idle => {
-              println!("target already running");
+              eprintln!("target already running");
               thread::sleep(time::Duration::from_millis(5000));
               continue;
             }
             _ => {
               // any other case...
-              println!("spawning process {}", target_process_name);
+              eprintln!("spawning process {}", target_process_name);
               Command::new(target_process_name)
                 .spawn()
                 .expect("failed to start target process");
@@ -40,7 +40,7 @@ fn main() {
         {
           match tp.status() {
             ProcessStatus::Run => {
-              println!(
+              eprintln!(
                 "target {} already running. {} instances already.",
                 target_process_path,
                 target_processes.len()
@@ -51,8 +51,10 @@ fn main() {
           }
         }
       } else {
-        println!("spawning process {}", target_process_path);
-        Command::new(target_process_path)
+        eprintln!("spawning process {}", target_process_path);
+        let p = std::path::Path::new("C:\\Program Files\\obs-studio\\bin\\64bit");
+        std::env::set_current_dir(&p);
+        Command::new(target_process_path).args(&["--startreplaybuffer"])
           .spawn()
           .expect("failed to start target process");
         // so i don't blow my computer up with ~15GB of chrome instances starting as fast as possible
