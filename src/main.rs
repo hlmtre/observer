70d6c06001fa6b0446ad5e7001e4920d5eb5bb77
Matt Weller <hellmitre@gmail.com>
2020-10-22 21:49:10 -0700

    pulls from a config and accepts args for target process.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 extern crate sysinfo;
 
-use std::fs::File;
 use std::io;
 use std::io::Error;
 use std::io::ErrorKind;
 use std::io::{BufRead, BufReader};
+use std::{fs::File, process};
 use std::{process::Command, thread, time};
 use sysinfo::{ProcessExt, ProcessStatus, RefreshKind, SystemExt};
+use time::Duration;
 
 #[derive(Default, Debug)]
 struct Obs {
   trigger_process_name: String,
   target_process_path: String,
   target_process_name: String,
+  target_args: String,
 }
 
 impl Obs {
   fn is_valid(&self) -> bool {
-    if self.trigger_process_name.len() > 0
-      && self.target_process_name.len() > 0
-      && self.target_process_path.len() > 0
-    {
+    if self.target_process_name.len() > 0 && self.target_process_path.len() > 0 {
       return true;
     }
     return false;
@@ -28,8 +27,20 @@ impl Obs {
 }
 
 fn main() {
-  let args: Vec<String> = std::env::args().collect();
-  let o: Obs = open_config(args[1].as_str()).unwrap();
+  let mut args: Vec<String> = std::env::args().collect();
+  if args.len() < 2 {
+    // assume it's here, and it's called observer.conf
+    args.push("observer.conf".to_string());
+  }
+  let obs_or_err = open_config(args[1].as_str());
+  let o = match obs_or_err {
+    Ok(k) => k,
+    Err(e) => {
+      eprintln!("couldn't find assumed ./observer.conf. error: {}", e);
+      process::exit(1);
+    }
+  };
+
   let trigger_process_name = o.trigger_process_name.replace("\"", "");
   let f = o.target_process_path.replace("\"", "");
   let target_process_path = f.trim().to_string();
@@ -43,22 +54,30 @@ fn main() {
     // prevent temporary value dropped while borrowed
     let processes = s.get_process_by_name(tr);
     let target_processes = s.get_process_by_name(ta);
-    eprintln!("looking for process {}", tr);
+    if cfg!(debug_assertions) {
+      eprintln!("looking for process {}", tr);
+    }
     if processes.len() > 0 {
-      eprintln!("found process {}!", tr);
+      if cfg!(debug_assertions) {
+        eprintln!("found process {}!", tr);
+      }
       if target_processes.len() > 0 {
         let tp = target_processes[0]; // we're guaranteed to have at least one...
         #[cfg(target_os = "linux")]
         {
           match tp.status() {
             ProcessStatus::Run | ProcessStatus::Sleep | ProcessStatus::Idle => {
-              eprintln!("target already running");
-              thread::sleep(time::Duration::from_millis(5000));
+              if cfg!(debug_assertions) {
+                eprintln!("target already running");
+              }
+              thread::sleep(Duration::from_millis(5000));
               continue;
             }
             _ => {
               // any other case...
-              eprintln!("spawning process {}", target_process_name);
+              if cfg!(debug_assertions) {
+                eprintln!("spawning process {}", target_process_name);
+              }
               Command::new(target_process_name.clone())
                 .spawn()
                 .expect("failed to start target process");
@@ -70,32 +89,39 @@ fn main() {
         {
           match tp.status() {
             ProcessStatus::Run => {
-              eprintln!(
-                "target {} already running. {} instances already.",
-                target_process_path,
-                target_processes.len()
-              );
-              thread::sleep(time::Duration::from_millis(5000));
+              if cfg!(debug_assertions) {
+                eprintln!(
+                  "target {} already running. {} instances already.",
+                  target_process_path,
+                  target_processes.len()
+                );
+              }
+              thread::sleep(Duration::from_millis(5000));
               continue;
             }
           }
         }
       } else {
-        eprintln!("spawning process {}", target_process_path);
-        //let p = std::path::Path::new(target_process_path.as_str());
-        //std::env::set_current_dir(&p);
-        eprintln!("{:#?}", target_process_path);
-        Command::new(target_process_path.clone())
-          //.args(&["--startreplaybuffer"])
-          .spawn()
-          .expect("failed to start target process");
-        // so i don't blow my computer up with ~15GB of chrome instances starting as fast as possible
         if cfg!(debug_assertions) {
-          thread::sleep(time::Duration::from_millis(1000));
+          eprintln!("spawning process {}", target_process_path);
         }
+        let mut a: Vec<&str> = Vec::new();
+        let pdir = std::path::Path::new(target_process_path.as_str())
+          .parent()
+          .unwrap();
+        let _ = std::env::set_current_dir(pdir);
+        let mut c = Command::new(target_process_path.clone());
+        if o.target_args.len() > 0 {
+          // if arguments are specified in the config file
+          let s = o.target_args.replace("\"", "");
+          a.append(s.split(" ").collect::<Vec<&str>>().as_mut());
+          c.args(a);
+        }
+        c.spawn().expect("ohtehnoes");
+        thread::sleep(Duration::from_millis(1000));
       }
     } else {
-      thread::sleep(time::Duration::from_millis(5000));
+      thread::sleep(Duration::from_millis(5000));
     }
   }
 }
@@ -109,16 +135,19 @@ fn open_config(path: &str) -> Result<Obs, io::Error> {
     if a.starts_with("trigger_process_name") {
       let el: Vec<&str> = a.split("=").collect();
       o.trigger_process_name = el[1].to_string();
-    } else if a.starts_with("target_process_name") {
-      let el: Vec<&str> = a.split("=").collect();
-      o.target_process_name = el[1].to_string();
     } else if a.starts_with("target_process_path") {
       let el: Vec<&str> = a.split("=").collect();
       o.target_process_path = el[1].to_string();
+    } else if a.starts_with("target_args") {
+      let el: Vec<&str> = a.split("=").collect();
+      o.target_args = el[1].to_string();
     }
+  }
+  if o.target_process_path.len() > 0 {
+    o.target_process_name = o.target_process_path.clone()
   }
   if !o.is_valid() {
     return Err(Error::new(ErrorKind::Other, "invalid config!"));
   }
-  return Ok(o);
+  Ok(o)
 }
