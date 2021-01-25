8335321855780c021c83617dca66592b078a6649
Matt Weller <hellmitre@gmail.com>
2021-01-24 22:18:24 -0800

    move string cleanup into opening function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate sysinfo;
 
+use std::path::Path;
 use std::{
   fs::File,
   io::{self, BufRead, BufReader, Error, ErrorKind},
@@ -19,7 +20,7 @@ struct Obs {
 
 impl Obs {
   fn is_valid(&self) -> bool {
-    if self.target_process_name.len() > 0 && self.trigger_process_name.len() > 0 {
+    if self.target_process_path.len() > 0 && self.trigger_process_name.len() > 0 {
       return true;
     }
     return false;
@@ -43,24 +44,21 @@ fn main() {
 
   // all this string type juggling is to
   // prevent temporary value dropped while borrowed
-  let trigger_process_name = o.trigger_process_name.replace("\"", "");
-  let f = o.target_process_path.replace("\"", "");
-  let target_process_path = f.trim().to_string();
-  let target_process_name = o.target_process_name.replace("\"", "");
-  let tr: &str = trigger_process_name.trim();
-  let ta: &str = target_process_name.trim();
   let r = RefreshKind::new();
   let mut s = sysinfo::System::new_with_specifics(r.with_processes());
   loop {
     s.refresh_processes();
-    let processes = s.get_process_by_name(tr);
-    let target_processes = s.get_process_by_name(ta);
+    let processes = s.get_process_by_name(o.trigger_process_name.as_str());
+    let target_processes = s.get_process_by_name(o.target_process_name.as_str());
     if cfg!(debug_assertions) {
-      eprintln!("looking for process {}; would spawn {}", tr, ta);
+      eprintln!(
+        "looking for process {}; would spawn {}",
+        o.trigger_process_name, o.target_process_name
+      );
     }
     if processes.len() > 0 {
       if cfg!(debug_assertions) {
-        eprintln!("found process {}!", tr);
+        eprintln!("found process {}!", o.trigger_process_name);
       }
       if target_processes.len() > 0 {
         let tp = target_processes[0]; // we're guaranteed to have at least one...
@@ -77,9 +75,9 @@ fn main() {
             _ => {
               // any other case...
               if cfg!(debug_assertions) {
-                eprintln!("spawning process {}", target_process_name);
+                eprintln!("spawning process {}", o.target_process_name);
               }
-              Command::new(target_process_name.clone())
+              Command::new(o.target_process_path.clone())
                 .spawn()
                 .expect("failed to start target process");
             }
@@ -93,7 +91,7 @@ fn main() {
               if cfg!(debug_assertions) {
                 eprintln!(
                   "target {} already running. {} instances already.",
-                  target_process_path,
+                  o.target_process_path,
                   target_processes.len()
                 );
               }
@@ -104,14 +102,14 @@ fn main() {
         }
       } else {
         if cfg!(debug_assertions) {
-          eprintln!("spawning process {}", target_process_path);
+          eprintln!("spawning process {}", o.target_process_path);
         }
         let mut a: Vec<&str> = Vec::new();
-        let pdir = std::path::Path::new(target_process_path.as_str())
+        let pdir = std::path::Path::new(o.target_process_path.as_str())
           .parent()
           .unwrap();
         let _ = std::env::set_current_dir(pdir);
-        let mut c = Command::new(target_process_path.clone());
+        let mut c = Command::new(o.target_process_path.clone());
         // if arguments are specified in the config file
         if o.target_args.len() > 0 {
           let s = o.target_args.replace("\"", "");
@@ -134,25 +132,45 @@ fn open_config(path: &str) -> Result<Obs, io::Error> {
   let mut o = Obs::default();
   let f = File::open(path)?;
   let b = BufReader::new(f);
+  let mut trigger_process_name = String::new();
+  let mut target_process_path = String::new();
+  let mut target_args = String::new();
   for l in b.lines() {
     let a = l.unwrap();
-    if a.starts_with("target_process_name") {
+    if a.starts_with("trigger_process_name") {
       let el: Vec<&str> = a.split("=").collect();
-      o.target_process_name = el[1].to_string();
-    } else if a.starts_with("trigger_process_name") {
-      let el: Vec<&str> = a.split("=").collect();
-      o.trigger_process_name = el[1].to_string();
+      trigger_process_name = el[1].to_string();
     } else if a.starts_with("target_process_path") {
       let el: Vec<&str> = a.split("=").collect();
-      o.target_process_path = el[1].to_string();
+      target_process_path = el[1].to_string();
     } else if a.starts_with("target_args") {
       let el: Vec<&str> = a.split("=").collect();
-      o.target_args = el[1].to_string();
+      target_args = el[1].to_string();
     }
   }
-  //if o.target_process_path.len() > 0 {
-  //  o.target_process_name = o.target_process_path.clone()
-  //}
+  let trigger_process_name = trigger_process_name.replace("\"", "");
+  let target_process_exe = std::ffi::OsString::from(
+    Path::new(target_process_path.replace("\"", "").trim())
+      .file_name()
+      .unwrap(),
+  )
+  .into_string()
+  .unwrap();
+  o.trigger_process_name = trigger_process_name.trim().to_string();
+  o.target_process_path = target_process_path.replace("\"", "").trim().to_string();
+  o.target_process_name = target_process_exe.clone();
+  o.target_args = target_args;
+
+  if cfg!(debug_assertions) {
+    eprintln!(
+      "Config: 
+        trigger process: {:#?},
+        target process path: {:#?},
+        target process name: {:#?}",
+      o.trigger_process_name, o.target_process_path, o.target_process_name
+    );
+  }
+
   if !o.is_valid() {
     return Err(Error::new(ErrorKind::Other, "invalid config!"));
   }
